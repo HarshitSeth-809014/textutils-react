21fabaf84d07e965daf046f6f55818646d088d5f
Harshit Seth <78683955+HarshitSeth-809014@users.noreply.github.com>
2021-09-15 12:25:04 +0530

    Added text summary

--- src/Components/TextForm.jsx
+++ src/Components/TextForm.jsx
@@ -5,16 +5,27 @@ const TextForm = ({ heading }) => {
     const [text, setText] = useState('');
 
     return (
-        <div>
-            <h1>{heading}</h1>
-            <div className="mb-3">
-                <textarea className="form-control" id="myBox" rows="8" placeholder="Enter the text here" value={text} onChange={(e) => setText(e.target.value)}></textarea>
+        <>
+            <div className="container">
+                <h1>{heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" id="myBox" rows="8" placeholder="Enter the text here" value={text} onChange={(e) => setText(e.target.value)}></textarea>
+                </div>
+                <div className="btn-group" role="group" aria-label="Basic outlined example">
+                    <button type="button" className="btn btn-outline-primary" onClick={() => setText(text.toUpperCase())}>Convert to Uppercase</button>
+                    <button type="button" className="btn btn-outline-primary" onClick={() => setText(text.toLowerCase())}>Convert to Lowercase</button>
+                </div>
             </div>
-            <div className="btn-group" role="group" aria-label="Basic outlined example">
-                <button type="button" className="btn btn-outline-primary" onClick={() => setText(text.toUpperCase())}>Convert to Uppercase</button>
-                <button type="button" className="btn btn-outline-primary" onClick={() => setText(text.toLowerCase())}>Convert to Lowercase</button>
+
+            <div className="container my-4">
+                <h2>Your text summary</h2>
+                <p>{text.split(" ").length} words and {text.length} characters</p>
+                <p>{(0.0032 * text.split(" ").length).toFixed(2)} minutes to read</p>
+
+                <h3>Preview</h3>
+                <p>{text}</p>
             </div>
-        </div>
+        </>
     )
 }
 
