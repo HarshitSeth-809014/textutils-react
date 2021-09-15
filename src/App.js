2f876e89658e9c93c6f82686cf418c3bb0037920
Harshit Seth <78683955+HarshitSeth-809014@users.noreply.github.com>
2021-09-15 10:55:31 +0530

    Navbar Added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from "./Components/Navbar";
+
+function App() {
+  const name = "TextUtils"
+
+  return (
+    <>
+      <Navbar title={name}/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react'
+
+const Navbar = ({title}) => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">{title}</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">About</a>
+                        </li>
+                    </ul>
+                    <form className="d-flex">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link" href="https://github.com/HarshitSeth-809014" target="_blank" rel="noreferrer">My Github</a>
+                            </li>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
