5b6519cdbd00794d324a1065dedc9ae6cd1813dc
Harshit Seth <78683955+HarshitSeth-809014@users.noreply.github.com>
2021-09-17 13:14:46 +0530

    Added Alert

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import About from "./Components/About";
+import Alert from "./Components/Alert";
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
+import React from 'react'
 
 function App() {
   const name = "TextUtils"
+  const [alert, setAlert] = React.useState(null)
+
+  const showAlert = (mess, type) => {
+    setAlert({
+      msg: mess,
+      type: type
+    })
+    setTimeout(() => setAlert(null), 1500);
+  }
 
   return (
     <>
       <Navbar title={name} />
+      <Alert alert={alert} />
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze"/>
-        {/* <About /> */}
+        <TextForm heading="Enter the text to analyze" showAlert={showAlert} />
       </div>
     </>
   );
--- src/Components/TextForm.jsx
+++ src/Components/TextForm.jsx
@@ -1,7 +1,6 @@
-/* eslint-disable no-empty-character-class */
 import React, { useState } from 'react'
 
-const TextForm = ({ heading }) => {
+const TextForm = ({ heading, showAlert }) => {
 
     const [text, setText] = useState('');
 
@@ -13,11 +12,30 @@ const TextForm = ({ heading }) => {
                     <textarea className="form-control" id="myBox" rows="8" placeholder="Enter the text here" value={text} onChange={(e) => setText(e.target.value)}></textarea>
                 </div>
                 <div className="btn-group" role="group" aria-label="Basic outlined example">
-                    <button type="button" className="btn btn-outline-primary" onClick={() => setText(text.toUpperCase())}>Convert to Uppercase</button>
-                    <button type="button" className="btn btn-outline-primary" onClick={() => setText(text.toLowerCase())}>Convert to Lowercase</button>
-                    <button type="button" className="btn btn-outline-primary" onClick={() => setText('')}>Clear Text</button>
-                    <button type="button" className="btn btn-outline-primary" onClick={() => setText(text.trim())}>Trim Whitespaces</button>
-                    <button type="button" className="btn btn-outline-primary" onClick={() => navigator.clipboard.writeText(document.getElementById('myBox').value)}>Copy Text</button> 
+                    <button type="button" className="btn btn-outline-primary" onClick={() => {
+                        setText(text.toUpperCase())
+                        showAlert("Converted to Uppercase!", "success")
+                    }}>Convert to Uppercase</button>
+
+                    <button type="button" className="btn btn-outline-primary" onClick={() => {
+                        setText(text.toLowerCase())
+                        showAlert("Converted to Lowercase!", "success")
+                    }}>Convert to Lowercase</button>
+
+                    <button type="button" className="btn btn-outline-primary" onClick={() => {
+                        setText('')
+                        showAlert("Text cleared successfully!", "success")
+                    }}>Clear Text</button>
+
+                    <button type="button" className="btn btn-outline-primary" onClick={() => {
+                        setText(text.trim())
+                        showAlert("Whitespaces trimmed successfully!", "success")
+                    }}>Remove Whitespaces</button>
+
+                    <button type="button" className="btn btn-outline-primary" onClick={() => {
+                        navigator.clipboard.writeText(document.getElementById('myBox').value)
+                        showAlert("Text copied successfully!", "success")
+                    }}>Copy Text</button>
                 </div>
             </div>
 
