e5ab70738bee8906c146058340d052601f076382
Harshit Seth <78683955+HarshitSeth-809014@users.noreply.github.com>
2021-09-21 13:33:51 +0530

    Added Router and used

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import Alert from "./Components/Alert";
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
+import About from "./Components/About";
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   const name = "TextUtils"
@@ -16,13 +22,24 @@ function App() {
   }
 
   return (
-    <>
+    <Router>
+
       <Navbar title={name} />
       <Alert alert={alert} />
+
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze" showAlert={showAlert} />
+        <Switch>
+          <Route exact path="/">
+            <TextForm heading="Enter the text to analyze" showAlert={showAlert} />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
       </div>
-    </>
+
+
+    </Router>
   );
 }
 
